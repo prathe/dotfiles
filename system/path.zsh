0b983bbbdca1d43e43af16447fe604d0398d1ca1
Philippe Rathé <prathe@gmail.com>
2011-11-05 13:54:48 -0400

    Add ~/bin in PATH

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH=".:bin:$HOME/.rbenv/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export PATH=".:bin:$HOME/.rbenv/bin:$HOME/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:$MANPATH"
