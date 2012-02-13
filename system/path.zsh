40b0fc73472db02c8faf7ebea734c38d18539925
Philippe Rathé <prathe@gmail.com>
2012-02-12 19:26:13 -0500

    Add Arduino tools to PATH

--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,5 @@
 export PATH="$HOME/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export PATH="`brew --prefix php`/bin:$PATH"
+export PATH="$PATH:/Applications/Arduino.app/Contents/Resources/Java/hardware/tools/avr/bin"
 
 export MANPATH="/usr/local/man:$MANPATH"
