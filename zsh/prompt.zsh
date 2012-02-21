53b0b84b667b772a4a20ec9c6cc654e7f8dd90a3
Philippe Rathé <prathe@gmail.com>
2012-02-21 09:43:31 -0500

    Enhance unspushed

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -28,7 +28,8 @@ git_prompt_info () {
 }
 
 unpushed () {
-  /usr/bin/git cherry -v `/usr/bin/git config --get branch.master.remote`/$(git_branch) 2>/dev/null
+  remote=`/usr/bin/git config --get branch.$(git_branch).remote || echo 'origin'`
+  /usr/bin/git cherry -v $remote/$(git_branch) 2>/dev/null
 }
 
 need_push () {
