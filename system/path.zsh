9ed0f92115a9bca884a5749fedc1b6c4c39bc808
Philippe Rathé <prathe@gmail.com>
2014-11-05 10:35:01 -0500

    no more php warning on zsh

--- system/path.zsh
+++ system/path.zsh
@@ -1,5 +1,4 @@
 export PATH="$HOME/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
-export PATH="`brew --prefix php`/bin:$PATH"
 export PATH="$PATH:/Applications/Arduino.app/Contents/Resources/Java/hardware/tools/avr/bin"
 export PATH="$M2:$PATH"
 
